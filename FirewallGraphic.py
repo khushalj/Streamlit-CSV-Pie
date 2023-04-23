086e24215617a9d37d6c79f3705b64e74fcc97e1
Khushal Jain <khushalj@outlook.com>
2023-04-23 20:24:24 +0530

    Update FirewallGraphic.py
    Sundar Tables

--- FirewallGraphic.py
+++ FirewallGraphic.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import pandas as pd
+
 def main():
     st.title("Firewall Status")
 
     # Upload CSV
     uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-        for i in range(len(df)):
-            name = df.iloc[i]['Name']
-            enabled = df.iloc[i]['Enabled']
-            if enabled:
-                st.write(name + " ✅")
-            else:
-                st.write(name + " ❌")
+
+        # Create a dictionary to map boolean values to checkmark or cross symbols
+        status_dict = {True: "✅", False: "❌"}
+
+        # Replace the boolean values with checkmark or cross symbols
+        df["Enabled"] = df["Enabled"].map(status_dict)
+
+        #Table BG
+        st.markdown(
+            '<style>div.row-widget.stRadio > div{background-color: #f4f4f4}</style>',
+            unsafe_allow_html=True,
+        )
+
+        # Display table
+        st.write(df)
+
 if __name__ == "__main__":
     main()
