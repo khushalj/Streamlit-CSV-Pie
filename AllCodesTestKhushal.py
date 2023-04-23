7e4cc8d904568935270e3a248a15fb62388f4d86
Khushal Jain <khushalj@outlook.com>
2023-04-24 00:11:43 +0530

    Update AllCodesTestKhushal.py
    Side panel and login issues resolved

--- AllCodesTestKhushal.py
+++ AllCodesTestKhushal.py
@@ -1,35 +1,34 @@
 import streamlit as st
+import pandas as pd
 
-# Login credentials
-LOGIN_ID = "securitybulls"
-PASSWORD = "7eae7644"
-
-# Define function for login page
+# Define login function to authenticate user
 def login():
-    st.title("Login")
+    st.sidebar.header("Login")
 
-    # Get login details from user
-    login_id = st.text_input("Login ID")
-    password = st.text_input("Password", type="password")
+    # Define credentials
+    LOGIN_ID = "securitybulls"
+    PASSWORD = "7eae7644"
 
-    # Check if login details are correct
-    if login_id == LOGIN_ID and password == PASSWORD:
-        st.success("Login successful!")
+    # Get user input
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+
+    # Verify credentials
+    if username == LOGIN_ID and password == PASSWORD:
         return True
     else:
-        st.error("Incorrect login details. Please try again.")
         return False
 
-# Define function for network audit page
+# Define functions to display different pages
 def network_audit():
     st.title("Network Audit")
-    sub_page = st.sidebar.radio("Select Sub-Page", ["Firewall Stats", "Open Port Logs", "Network Stats"])
+    menu = ["Firewall Stats", "Open Port Logs", "Network Stats"]
+    choice = st.sidebar.selectbox("Select an option", menu)
 
-    if sub_page == "Firewall Stats":
-        st.title("Firewall Status")
-
-        # Replace the link with the actual link to the CSV file
-        csv_link = "https://example.com/firewall_stats.csv"
+    # Display selected sub-page
+    if choice == "Firewall Stats":
+        st.subheader("Firewall Stats")
+        csv_link = "https://example.com/firewall_stats.csv" # Replace with actual link to CSV file
         df = pd.read_csv(csv_link)
 
         # Create a dictionary to map boolean values to checkmark or cross symbols
@@ -38,7 +37,7 @@ def network_audit():
         # Replace the boolean values with checkmark or cross symbols
         df["Enabled"] = df["Enabled"].map(status_dict)
 
-        #Table BG
+        # Table BG
         st.markdown(
             '<style>div.row-widget.stRadio > div{background-color: #f4f4f4}</style>',
             unsafe_allow_html=True,
@@ -47,58 +46,122 @@ def network_audit():
         # Display table
         st.write(df)
 
-    elif sub_page == "Open Port Logs":
-        st.title("Open Port Logs")
-        # Code to display open port logs goes here
+    elif choice == "Open Port Logs":
+        st.subheader("Open Port Logs")
+        st.write("Coming soon...")
 
-    elif sub_page == "Network Stats":
-        st.title("Network Stats")
-        # Code to display network stats goes here
+    elif choice == "Network Stats":
+        st.subheader("Network Stats")
+        st.write("Coming soon...")
 
-# Define function for OS audit page
 def os_audit():
     st.title("OS Audit")
-    sub_page = st.sidebar.radio("Select Sub-Page", ["OS Details", "System Version", "Peripheral Devices"])
+    menu = ["OS Details", "System Version", "Peripheral Devices"]
+    choice = st.sidebar.selectbox("Select an option", menu)
 
-    if sub_page == "OS Details":
-        st.title("OS Details")
-        # Code to display OS details goes here
+    # Display selected sub-page
+    if choice == "OS Details":
+        st.subheader("OS Details")
+        st.write("Coming soon...")
 
-    elif sub_page == "System Version":
-        st.title("System Version")
-        # Code to display system version goes here
+    elif choice == "System Version":
+        st.subheader("System Version")
+        st.write("Coming soon...")
 
-    elif sub_page == "Peripheral Devices":
-        st.title("Peripheral Devices")
-        # Code to display peripheral devices goes here
+    elif choice == "Peripheral Devices":
+        st.subheader("Peripheral Devices")
+        st.write("Coming soon...")
 
-# Define function for vulnerability assessment page
 def vulnerability_assessment():
     st.title("Vulnerability Assessment")
-    # Code to perform vulnerability assessment goes here
+    st.write("Coming soon...")
 
-# Define function for malware logs page
 def malware_logs():
     st.title("Malware Logs")
-    # Code to display malware logs goes here
+    st.write("Coming soon...")
 
-# Define the main function for the Streamlit app
+# Define main function to run the app
 def main():
-    if login():
-        # Create a sidebar with options to navigate to different pages
-        st.sidebar.title("Navigation")
-        pages = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
-        page = st.sidebar.radio("Go to", pages)
+    # Authenticate user
+    authenticated = login()
 
-        # Call the appropriate function based on the selected page
-        if page == "Network Audit":
+    # If authentication successful, display app
+    if authenticated:
+        st.sidebar.success("Logged in as securitybulls")
+
+        # Display sidebar menu
+        st.sidebar.title("Navigation")
+        menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
+        choice = st.sidebar.selectbox("Select a page", menu)
+
+        # Display selected page
+        if choice == "Network Audit":
             network_audit()
-        elif page == "OS Audit":
+        elif choice == "OS Audit":
             os_audit()
-        elif page == "Vulnerability Assessment":
+        elif choice == "Vulnerability Assessment":
             vulnerability_assessment()
-        elif page == "Malware Logs":
+        elif choice == "Malware Logs":
             malware_logs()
+    else:
+        st.sidebar.error("Incorrect username or password")
+
+        # Display login button
+        login_button = st.sidebar.button("Login")
+
+        # If login button clicked, re-attempt authentication
+        if login_button:
+            authenticated = login()
+            if authenticated
+# Define main function to run the app
+def main():
+    # Authenticate user
+    authenticated = login()
+
+    # If authentication successful, display app
+    if authenticated:
+        st.sidebar.success("Logged in as securitybulls")
+
+        # Display sidebar menu
+        st.sidebar.title("Navigation")
+        menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
+        choice = st.sidebar.selectbox("Select a page", menu)
+
+        # Display selected page
+        if choice == "Network Audit":
+            network_audit()
+        elif choice == "OS Audit":
+            os_audit()
+        elif choice == "Vulnerability Assessment":
+            vulnerability_assessment()
+        elif choice == "Malware Logs":
+            malware_logs()
+    else:
+        st.sidebar.error("Incorrect username or password")
+
+        # Display login button
+        login_button = st.sidebar.button("Login")
+
+        # If login button clicked, re-attempt authentication
+        if login_button:
+            authenticated = login()
+            if authenticated:
+                st.sidebar.success("Logged in as securitybulls")
+
+                # Display sidebar menu
+                st.sidebar.title("Navigation")
+                menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
+                choice = st.sidebar.selectbox("Select a page", menu)
+
+                # Display selected page
+                if choice == "Network Audit":
+                    network_audit()
+                elif choice == "OS Audit":
+                    os_audit()
+                elif choice == "Vulnerability Assessment":
+                    vulnerability_assessment()
+                elif choice == "Malware Logs":
+                    malware_logs()
 
 if __name__ == "__main__":
     main()
