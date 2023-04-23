3377cddb17a4f9c73c87172a09708023bf4f2822
Khushal Jain <khushalj@outlook.com>
2023-04-24 00:18:28 +0530

    Update AllCodesTestKhushal.py

--- AllCodesTestKhushal.py
+++ AllCodesTestKhushal.py
@@ -1,35 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-# Define login function to authenticate user
-def login():
-    st.sidebar.header("Login")
+def firewall_stats():
+    st.title("Firewall Status")
 
-    # Define credentials
-    LOGIN_ID = "securitybulls"
-    PASSWORD = "7eae7644"
+    # Upload CSV
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
-    # Get user input
-    username = st.sidebar.text_input("Username")
-    password = st.sidebar.text_input("Password", type="password")
-
-    # Verify credentials
-    if username == LOGIN_ID and password == PASSWORD:
-        return True
-    else:
-        return False
-
-# Define functions to display different pages
-def network_audit():
-    st.title("Network Audit")
-    menu = ["Firewall Stats", "Open Port Logs", "Network Stats"]
-    choice = st.sidebar.selectbox("Select an option", menu)
-
-    # Display selected sub-page
-    if choice == "Firewall Stats":
-        st.subheader("Firewall Stats")
-        csv_link = "https://example.com/firewall_stats.csv" # Replace with actual link to CSV file
-        df = pd.read_csv(csv_link)
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
 
         # Create a dictionary to map boolean values to checkmark or cross symbols
         status_dict = {True: "✅", False: "❌"}
@@ -37,7 +16,7 @@ def network_audit():
         # Replace the boolean values with checkmark or cross symbols
         df["Enabled"] = df["Enabled"].map(status_dict)
 
-        # Table BG
+        #Table BG
         st.markdown(
             '<style>div.row-widget.stRadio > div{background-color: #f4f4f4}</style>',
             unsafe_allow_html=True,
@@ -46,115 +25,38 @@ def network_audit():
         # Display table
         st.write(df)
 
-    elif choice == "Open Port Logs":
-        st.subheader("Open Port Logs")
-        st.write("Coming soon...")
-
-    elif choice == "Network Stats":
-        st.subheader("Network Stats")
-        st.write("Coming soon...")
-
-def os_audit():
-    st.title("OS Audit")
-    menu = ["OS Details", "System Version", "Peripheral Devices"]
-    choice = st.sidebar.selectbox("Select an option", menu)
-
-    # Display selected sub-page
-    if choice == "OS Details":
-        st.subheader("OS Details")
-        st.write("Coming soon...")
-
-    elif choice == "System Version":
-        st.subheader("System Version")
-        st.write("Coming soon...")
-
-    elif choice == "Peripheral Devices":
-        st.subheader("Peripheral Devices")
-        st.write("Coming soon...")
-
-def vulnerability_assessment():
-    st.title("Vulnerability Assessment")
-    st.write("Coming soon...")
-
-def malware_logs():
-    st.title("Malware Logs")
-    st.write("Coming soon...")
-
-# Define main function to run the app
-def main():
-    # Authenticate user
-    authenticated = login()
-
-    # If authentication successful, display app
-    if authenticated:
-        st.sidebar.success("Logged in as securitybulls")
-
-        # Display sidebar menu
-        st.sidebar.title("Navigation")
-        menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
-        choice = st.sidebar.selectbox("Select a page", menu)
-
-        # Display selected page
-        if choice == "Network Audit":
-            network_audit()
-        elif choice == "OS Audit":
-            os_audit()
-        elif choice == "Vulnerability Assessment":
-            vulnerability_assessment()
-        elif choice == "Malware Logs":
-            malware_logs()
-    else:
-        st.sidebar.error("Incorrect username or password")
-
-        # Display login button
-        login_button = st.sidebar.button("Login")
-
-        # If login button clicked, re-attempt authentication
-        if login_button:
-            authenticated = login()
-            if authenticated:
-        st.sidebar.success("Logged in as securitybulls")
-
-        # Display sidebar menu
-        st.sidebar.title("Navigation")
-        menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
-        choice = st.sidebar.selectbox("Select a page", menu)
-
-        # Display selected page
-        if choice == "Network Audit":
-            network_audit()
-        elif choice == "OS Audit":
-            os_audit()
-        elif choice == "Vulnerability Assessment":
-            vulnerability_assessment()
-        elif choice == "Malware Logs":
-            malware_logs()
-    else:
-        st.sidebar.error("Incorrect username or password")
-
-        # Display login button
-        login_button = st.sidebar.button("Login")
-
-        # If login button clicked, re-attempt authentication
-        if login_button:
-            authenticated = login()
-            if authenticated:
-                st.sidebar.success("Logged in as securitybulls")
-
-                # Display sidebar menu
-                st.sidebar.title("Navigation")
-                menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
-                choice = st.sidebar.selectbox("Select a page", menu)
-
-                # Display selected page
-                if choice == "Network Audit":
-                    network_audit()
-                elif choice == "OS Audit":
-                    os_audit()
-                elif choice == "Vulnerability Assessment":
-                    vulnerability_assessment()
-                elif choice == "Malware Logs":
-                    malware_logs()
-
 if __name__ == "__main__":
-    main()
+    st.set_page_config(page_title="Security Bulls App")
+
+    # Login Page
+    login_id = st.secrets["login_id"]
+    password = st.secrets["password"]
+    user_input_id = st.text_input("Enter Login ID")
+    user_input_password = st.text_input("Enter Password", type="password")
+    if st.button("Login"):
+        if user_input_id == login_id and user_input_password == password:
+            st.success("Logged In!")
+            menu = ["Network Audit", "OS Audit", "Vulnerability Assessment", "Malware Logs"]
+            choice = st.sidebar.selectbox("Select an option", menu)
+            if choice == "Network Audit":
+                network_audit_menu = ["Firewall Stats", "Open Port Logs", "Network Stats"]
+                network_audit_choice = st.sidebar.selectbox("Select a subpage", network_audit_menu)
+                if network_audit_choice == "Firewall Stats":
+                    firewall_stats()
+            elif choice == "OS Audit":
+                os_audit_menu = ["OS Details", "System Version", "Peripheral Devices"]
+                os_audit_choice = st.sidebar.selectbox("Select a subpage", os_audit_menu)
+                if os_audit_choice == "Firewall Stats":
+                    # Write code for OS Details subpage here
+                    pass
+        else:
+            st.error("Invalid Login ID or Password")
+                        elif choice == "Vulnerability Assessment":
+                # Write code for Vulnerability Assessment page here
+                pass
+            elif choice == "Malware Logs":
+                # Write code for Malware Logs page here
+                pass
+    else:
+        pass
+
