8e1f98df4e8478b37ae40d5162d7f777f9e15094
Khushal Jain <khushalj@outlook.com>
2023-04-24 11:57:13 +0530

    Update NewLogin.py

--- NewLogin.py
+++ NewLogin.py
@@ -5,7 +5,7 @@ CORRECT_USERNAME = "user"
 CORRECT_PASSWORD = "password"
 
 # Define login page
-def show_login():
+def show_login(redirect_url):
     # Show login form
     st.write("# Login")
     username = st.text_input("Username")
@@ -17,6 +17,8 @@ def show_login():
         if username == CORRECT_USERNAME and password == CORRECT_PASSWORD:
             st.session_state.logged_in = True
             st.experimental_rerun()
+            st.experimental_set_query_params(logged_in=True)
+            st.experimental_set_query_params(redirect_url=https://khushalj-streamlit-csv-pie-login-hg4xe8.streamlit.app/)
         else:
             st.error("Incorrect username or password")
 
@@ -30,6 +32,18 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
 if st.session_state.logged_in:
-    show_protected()
+    # Check if redirect URL is set
+    if "redirect_url" in st.experimental_get_query_params():
+        redirect_url = st.experimental_get_query_params()["redirect_url"][0]
+        st.experimental_set_query_params()
+        st.experimental_rerun()
+        st.write(f"Redirecting to {redirect_url}...")
+        # Add a small delay to allow the redirect message to display
+        st.experimental_sleep(1)
+        st.experimental_set_query_params()
+        st.experimental_rerun()
+        st.redirect(redirect_url)
+    else:
+        show_protected()
 else:
-    show_login()
+    show_login("https://www.google.com")
