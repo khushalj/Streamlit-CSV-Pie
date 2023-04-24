ef84726989387206c7b38dada0c4ae7cb2c943d1
Khushal Jain <khushalj@outlook.com>
2023-04-25 02:33:43 +0530

    Charts Customized

--- FirewallGraphic.py
+++ FirewallGraphic.py
@@ -2,22 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(page_title="Firewall Analysis")
+
 # Read the CSV file
 df = pd.read_csv("firewall_status.csv")
 
 # Add a new column with tick or cross marks based on the value in the "Enabled" column
-df["Status"] = df["Enabled"].apply(lambda x: "✔️" if x else "❌")
+df["Status"] = df["Enabled"].apply(lambda x: "✅" if x else "❌")
 
-# Display the CSV file as a table with tick or cross marks
-st.write(df[["Name", "Status"]].style.hide_index())
+# color of tick and cross marks based on the value in the "Enabled" column
+df["Status"] = df["Enabled"].apply(lambda x: "<span style='color:green'>✅</span>" if x else "<span style='color:red'>❌</span>")
 
-# Create a comparative bar chart for True and False values in the "Enabled" column
+# comparative bar chart for True and False values in the "Enabled" column
 enabled_count = df["Enabled"].value_counts()
-fig1 = px.bar(x=enabled_count.index, y=enabled_count.values, title="Firewall Value vs Number of Domains")
+colors = ["green", "red"]
+fig1 = px.bar(x=enabled_count.index, y=enabled_count.values, title="Comparative analysis of Active & Inactive Firewall on Domains", color=enabled_count.index, color_discrete_sequence=colors)
 fig1.update_xaxes(title_text="Firewall Value")
 fig1.update_yaxes(title_text="Number of Domains", tickmode='linear', dtick=1)
 st.plotly_chart(fig1)
 
 # Create a pie chart showing the frequency of True and False values in the "Enabled" column
-fig2 = px.pie(values=enabled_count.values, names=enabled_count.index, title="Enabled Pie Chart")
+fig2 = px.pie(values=enabled_count.values, names=enabled_count.index, title="Status of Firewall Across Domains", color=enabled_count.index, color_discrete_sequence=colors)
 st.plotly_chart(fig2)
+
+# Display the CSV file as a table with tick or cross 
+st.write(df[["Name", "Status"]].style.hide_index())
