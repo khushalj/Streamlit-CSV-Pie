6cfad524e56d517e5f828191a78d943df6300ae4
Khushal Jain <khushalj@outlook.com>
2023-04-25 05:01:30 +0530

    Update systeminfo.py

--- systeminfo.py
+++ systeminfo.py
@@ -1,22 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV file
-df = pd.read_csv('systeminfo.csv')
+# Read in the CSV file
+df = pd.read_csv("systeminfo.csv")
 
-# Group the data by Host name, Registered Owner, Registered Organization
-host_details = df[['Host Name', 'Registered Owner', 'Registered Organization']] \
-    .drop_duplicates() \
-    .reset_index(drop=True)
+# Group the columns and rename the headers
+host_details = df[["Host Name", "Registered Owner", "Registered Organization"]]
+host_details.columns = ["Host Name", "Owner", "Organization"]
 
-# Group the data by OS Name, OS Configuration, OS Version, OS Manufacturer, OS Build Type, Product ID, Original Install Date, System Boot Time
-os_properties = df[['OS Name', 'OS Configuration', 'OS Version', 'OS Manufacturer', 'OS Build Type', 'Product ID', 'Original Install Date', 'System Boot Time']] \
-    .drop_duplicates() \
-    .reset_index(drop=True)
+os_properties = df[["OS Name", "OS Configuration", "OS Version", "OS Manufacturer", "OS Build Type", "Product ID", "Original Install Date", "System Boot Time"]]
+os_properties.columns = ["OS Name", "OS Configuration", "OS Version", "OS Manufacturer", "OS Build Type", "Product ID", "Install Date", "Boot Time"]
 
-# Display the Host Details and OS Properties in separate tables
-st.write('## Host Details')
+# Display the data in tabular form
+st.write("## Host Details")
 st.table(host_details)
 
-st.write('## OS Properties')
+st.write("## OS Properties")
 st.table(os_properties)
