e4d039fb0bc7ade5e4e90e8f8a4e6585d0c0e51b
Khushal Jain <khushalj@outlook.com>
2023-04-25 01:22:15 +0530

    ReOrder Blame

--- ListnerPorts.py
+++ ListnerPorts.py
@@ -5,9 +5,6 @@ import plotly.express as px
 # Read the CSV file
 df = pd.read_csv("output.csv")
 
-# Display the CSV file as a table
-st.write(df)
-
 # Create a pie chart showing the frequency of each process
 process_count = df["Process"].value_counts()
 fig1 = px.pie(values=process_count, names=process_count.index, title="Process Frequency")
@@ -17,3 +14,6 @@ st.plotly_chart(fig1)
 address_count = df["LocalAddress"].value_counts()
 fig2 = px.bar(x=address_count.index, y=address_count.values, title="Local Address Frequency")
 st.plotly_chart(fig2)
+
+# Display the CSV file as a table
+st.write(df)
