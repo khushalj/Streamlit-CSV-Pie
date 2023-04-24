ca8e3a8b09e2fd2859314855122f5f5c911012c3
Khushal Jain <khushalj@outlook.com>
2023-04-25 04:05:46 +0530

    Tables Formed

--- systeminfo.py
+++ systeminfo.py
@@ -14,11 +14,9 @@ os_style = "<style>th {color: #3f88c5; font-size: 20px;}</style>"
 st.write(host_style, unsafe_allow_html=True)
 st.write(os_style, unsafe_allow_html=True)
 
-# Show the data in a vertical table
+# Show the data in a tabular format
 st.write('<h2 style="color: #f63366;">Host Details</h2>', unsafe_allow_html=True)
-for i, row in host_df.iterrows():
-    st.write(f'<b>Host {i+1}</b>', row.to_dict())
+st.table(host_df.style.set_properties(**{'font-size': '20px'}))
 
 st.write('<h2 style="color: #3f88c5;">OS Properties</h2>', unsafe_allow_html=True)
-for i, row in os_df.iterrows():
-    st.write(f'<b>OS {i+1}</b>', row.to_dict())
+st.table(os_df.style.set_properties(**{'font-size': '20px'}))
