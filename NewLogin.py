f3deeaa73a6ae5f63f33844af13bcfa6545d0c52
Khushal Jain <khushalj@outlook.com>
2023-04-24 19:43:28 +0530

    Update NewLogin.py

--- NewLogin.py
+++ NewLogin.py
@@ -18,20 +18,16 @@ def app():
     if st.button("Submit"):
         if user_input == username and pass_input == password:
             st.success("Login successful!")
-            st.write("Redirecting to website...")
-            # Redirect to website
-            st.experimental_set_query_params(token="abc123")
-            st.experimental_rerun()
+            st.write("Click the button to go to the website:")
+            # Display button with link to website
+            st.button("Go to website", on_click=open_website)
         else:
             st.error("Incorrect username or password")
 
-    # Check if user is authenticated
-    if st.session_state.get("is_authenticated"):
-        js = "window.open('https://khushalj-streamlit-csv-pie-login-hg4xe8.streamlit.app/','_blank')"
-        html = '<img src onerror="{}">'.format(js)
-        div = '<div style="display:none">{}</div>'.format(html)
-        st.markdown(div, unsafe_allow_html=True)
-        st.session_state.pop("is_authenticated")
+# Function to open website
+def open_website():
+    st.experimental_set_query_params(token="abc123")
+    st.experimental_redirect("https://khushalj-streamlit-csv-pie-newlogin-32ttdc.streamlit.app/")
 
 # Run the Streamlit app
 if __name__ == '__main__':
