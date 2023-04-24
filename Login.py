1957c473afc07ef3026a257855bc659019d16eed
Aishworyann <75421318+aishworyann@users.noreply.github.com>
2023-04-25 05:24:48 +0530

    updated search lottie animation

--- Login.py
+++ Login.py
@@ -18,7 +18,9 @@ def load_lottieurl(url: str):
     if r.status_code != 200:
         return None
     return r.json()
-lottie_url_search = "https://assets3.lottiefiles.com/packages/lf20_1PD1tpvlop.json"
+
+lottie_url_search = "https://assets7.lottiefiles.com/packages/lf20_yJ8wNO.json"
+# lottie_url_search = "https://assets3.lottiefiles.com/packages/lf20_1PD1tpvlop.json"
 lottie_url_hello = "https://assets3.lottiefiles.com/packages/lf20_nOfkZot060.json"
 lottie_url_welcome= "https://assets1.lottiefiles.com/packages/lf20_llbjwp92qL.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
