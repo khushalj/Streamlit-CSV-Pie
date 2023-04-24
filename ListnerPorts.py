753876161804fda471d4005f9c970b573c56e7dc
Khushal Jain <khushalj@outlook.com>
2023-04-25 01:28:15 +0530

    Formatting

--- ListnerPorts.py
+++ ListnerPorts.py
@@ -2,18 +2,23 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Read the CSV file
+# CSV Padhle bhai
 df = pd.read_csv("output.csv")
 
-# Create a pie chart showing the frequency of each process
+st.set_page_config(page_title="Listening Port Stats")
+
+# Bold Bold headings dekhlo
+st.markdown("<h1 style='text-align: center; font-weight: bold;'>LocalAddress Listening Port Stats</h1>", unsafe_allow_html=True)
+
+# Gol Gol Pie Dekho
 process_count = df["Process"].value_counts()
 fig1 = px.pie(values=process_count, names=process_count.index, title="Process Frequency")
 st.plotly_chart(fig1)
 
-# Create a bar chart showing the frequency of each local address
+# Bar Chart 🌚
 address_count = df["LocalAddress"].value_counts()
 fig2 = px.bar(x=address_count.index, y=address_count.values, title="Local Address Frequency")
 st.plotly_chart(fig2)
 
-# Display the CSV file as a table
-st.write(df)
+# Table m sab dekhlo
+st.write(df.style.set_properties(**{'font-weight': 'bold'}))
