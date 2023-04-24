c3b446065e8904e66e080869b9cbbfb139af06c4
Khushal Jain <khushalj@outlook.com>
2023-04-25 04:07:30 +0530

    Update systeminfo.py

--- systeminfo.py
+++ systeminfo.py
@@ -9,14 +9,18 @@ host_df = df[['Host Name', 'Registered Owner', 'Registered Organization']]
 os_df = df[['OS Name', 'OS Configuration', 'OS Version', 'OS Manufacturer', 'OS Build Type', 'Product ID', 'Original Install Date', 'System Boot Time']]
 
 # Set column headers and styles
-host_style = "<style>th {color: #f63366; font-size: 20px;}</style>"
-os_style = "<style>th {color: #3f88c5; font-size: 20px;}</style>"
+host_style = "<style>h2 {color: #f63366;}</style>"
+os_style = "<style>h2 {color: #3f88c5;}</style>"
 st.write(host_style, unsafe_allow_html=True)
 st.write(os_style, unsafe_allow_html=True)
 
 # Show the data in a tabular format
-st.write('<h2 style="color: #f63366;">Host Details</h2>', unsafe_allow_html=True)
-st.table(host_df.style.set_properties(**{'font-size': '20px'}))
+col1, col2 = st.beta_columns(2)
 
-st.write('<h2 style="color: #3f88c5;">OS Properties</h2>', unsafe_allow_html=True)
-st.table(os_df.style.set_properties(**{'font-size': '20px'}))
+with col1:
+    st.write('<h2>Host Details</h2>', unsafe_allow_html=True)
+    st.table(host_df.style.set_properties(**{'font-size': '20px'}))
+
+with col2:
+    st.write('<h2>OS Properties</h2>', unsafe_allow_html=True)
+    st.table(os_df.style.set_properties(**{'font-size': '20px'})))
