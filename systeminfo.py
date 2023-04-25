04d746d3ff511d9e7c9b3aceaf9207402622ea40
Khushal Jain <khushalj@outlook.com>
2023-04-25 05:57:37 +0530

    Bug fix

--- systeminfo.py
+++ systeminfo.py
@@ -13,7 +13,8 @@ sections = {
     'Locale Properties': ['System Locale', 'Input Locale', 'Time Zone'],
     'Memory Properties': ['Total Physical Memory', 'Available Physical Memory', 'Virtual Memory', 'Virtual Memory', 'Virtual Memory'],
     'Network & Network Adapter Properties': ['Domain', 'Logon Server', 'Hotfix(s)', '[01]', '[02]', '[03]', '[04]', 'Network Card(s)', '[01]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', 'Connection Name', 'Status', '[03]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', '[04]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', '[05]', 'Connection Name', 'DHCP Enabled', 'IP address(es)', '[01]', '[02]', '[06]', 'Connection Name', 'Status', '[07]', 'Connection Name', 'DHCP Enabled', 'IP address(es)'],
-    'hypervisor': ['Hyper-V Requirements']
+    'hypervisor': ['Hyper-V Requirements'],
+    'Hotfix Details': ['Hotfix(s)', '[01]', '[02]', '[03]', '[04]']
 }
 
 # create dictionary to store values for each section
@@ -34,4 +35,4 @@ for section, section_names in sections.items():
     section_df = pd.DataFrame(section_values[section].items(), columns=['Name', 'Value'])
     section_df = section_df.loc[section_df['Name'].isin(section_names)]
     section_df['Value'] = section_df['Value'].apply(lambda x: '✔️' if x == 'True' else ('❌' if x == 'False' else x)) # add tick or cross for True/False values
-    st.dataframe(section_df.style.set_properties(**{'width': '100px'}).set_caption(' ').set_table_styles([{'selector': 'th', 'props': [('background-color', '#D3D3D3'), ('color', 'black'), ('font-weight', 'bold'), ('font-size', '120%')]}] , width=800)
+    st.dataframe(section_df.style.set_properties(**{'width': '100px'}), width=800)
