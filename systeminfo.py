3be9cfe072bb0cbfbd54d473b7decf65b1238119
Khushal Jain <khushalj@outlook.com>
2023-04-25 06:04:08 +0530

    Update systeminfo.py

--- systeminfo.py
+++ systeminfo.py
@@ -13,7 +13,7 @@ sections = {
     'Locale Properties': ['System Locale', 'Input Locale', 'Time Zone'],
     'Memory Properties': ['Total Physical Memory', 'Available Physical Memory', 'Virtual Memory', 'Virtual Memory', 'Virtual Memory'],
     'Network & Network Adapter Properties': ['Domain', 'Logon Server', 'Hotfix(s)', '[01]', '[02]', '[03]', '[04]', 'Network Card(s)', '[01]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', 'Connection Name', 'Status', '[03]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', '[04]', 'Connection Name', 'DHCP Enabled', 'DHCP Server', 'IP address(es)', '[01]', '[02]', '[05]', 'Connection Name', 'DHCP Enabled', 'IP address(es)', '[01]', '[02]', '[06]', 'Connection Name', 'Status', '[07]', 'Connection Name', 'DHCP Enabled', 'IP address(es)'],
-    'hypervisor': ['Hyper-V Requirements'],
+    'Hypervisor': ['Hyper-V Requirements'],
     'Hotfix Details': ['Hotfix(s)', '[01]', '[02]', '[03]', '[04]']
 }
 
