5a0ad18d794f60467ef4b977f23444b2a269c856
Khushal Jain <khushalj@outlook.com>
2023-04-25 05:34:53 +0530

    Added more properties

--- systeminfo.py
+++ systeminfo.py
@@ -1,45 +1,54 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV file
-df = pd.read_csv("systeminfo.csv")
+# Load data from CSV
+df = pd.read_csv("systeminfo.csv", header=None, names=["Name", "Value"])
 
-# Group the details of the Host name, Registered Owner, and Registered Organization together
-host_details = df.loc[df['Name'].isin(['Host Name', 'Registered Owner', 'Registered Organization'])]
+# Define groups of properties
+host_details = ["Host Name", "Registered Owner", "Registered Organization"]
+os_properties = ["OS Name", "OS Configuration", "OS Version", "OS Manufacturer", "OS Build Type", "Product ID", "Original Install Date", "System Boot Time"]
+hardware_properties = ["System Manufacturer", "System Model", "System Type", "Processor(s)", "[01]", "Boot Device"]
+logical_properties = ["Windows Directory", "System Directory", "BIOS Version"]
+locale_properties = ["System Locale", "Input Locale", "Time Zone"]
+memory_properties = ["Total Physical Memory", "Available Physical Memory", "Virtual Memory", "Virtual Memory", "Virtual Memory"]
+network_properties = ["Domain", "Logon Server", "Hotfix(s)", "[01]", "[02]", "[03]", "[04]", "Network Card(s)", "[01]", "Connection Name", "DHCP Enabled", "DHCP Server", "IP address(es)", "[01]", "[02]", "Connection Name", "Status", "[03]", "Connection Name", "DHCP Enabled", "DHCP Server", "IP address(es)", "[01]", "[02]", "[04]", "Connection Name", "DHCP Enabled", "DHCP Server", "IP address(es)", "[01]", "[02]", "[05]", "Connection Name", "DHCP Enabled", "IP address(es)", "[01]", "[02]", "[06]", "Connection Name", "Status", "[07]", "Connection Name", "DHCP Enabled", "IP address(es)"]
 
-# Group the details of the OS Name, OS Configuration, OS Version, OS Manufacturer, OS Build Type, Product ID, Original Install Date, and System Boot Time together
-os_properties = df.loc[df['Name'].isin(['OS Name', 'OS Configuration', 'OS Version', 'OS Manufacturer', 'OS Build Type', 'Product ID', 'Original Install Date', 'System Boot Time'])]
+# Create separate dataframes for each group
+host_details_df = df[df["Name"].isin(host_details)]
+os_properties_df = df[df["Name"].isin(os_properties)]
+hardware_properties_df = df[df["Name"].isin(hardware_properties)]
+logical_properties_df = df[df["Name"].isin(logical_properties)]
+locale_properties_df = df[df["Name"].isin(locale_properties)]
+memory_properties_df = df[df["Name"].isin(memory_properties)]
+network_properties_df = df[df["Name"].isin(network_properties)]
 
-# Group the details of the System Manufacturer, System Model, System Type, Processor(s), [01], Boot Device together
-hardware_properties = df.loc[df['Name'].isin(['System Manufacturer', 'System Model', 'System Type', 'Processor(s)', '[01]', 'Boot Device'])]
+# Set page title
+st.set_page_config(page_title="System Info")
 
-# Group the details of the Windows Directory, System Directory, and BIOS Version together
-logical_properties = df.loc[df['Name'].isin(['Windows Directory', 'System Directory', 'BIOS Version'])]
+# Display host details table
+st.write("## Host Details")
+st.dataframe(host_details_df.style.set_properties(**{"width": "100px"}), height=300)
 
-# Group the details of the System Locale, Input Locale, and Time Zone together
-locale_properties = df.loc[df['Name'].isin(['System Locale', 'Input Locale', 'Time Zone'])]
+# Display OS properties table
+st.write("## OS Properties")
+st.dataframe(os_properties_df.style.set_properties(**{"width": "100px"}), height=400)
 
-# Group the details of the Total Physical Memory, Available Physical Memory, and Virtual Memory together
-memory_properties = df.loc[df['Name'].isin(['Total Physical Memory', 'Available Physical Memory', 'Virtual Memory', 'Virtual Memory', 'Virtual Memory'])]
+# Display hardware properties table
+st.write("## Hardware Properties")
+st.dataframe(hardware_properties_df.style.set_properties(**{"width": "100px"}), height=400)
 
-# Set the page title and width
-st.set_page_config(page_title="System Information", layout="wide")
+# Display logical properties table
+st.write("## Logical Properties")
+st.dataframe(logical_properties_df.style.set_properties(**{"width": "100px"}), height=200)
 
-# Display the tables with headings and same width
-st.write("Host Details")
-st.dataframe(host_details, width=600, height=300)
+# Display locale properties table
+st.write("## Locale Properties")
+st.dataframe(locale_properties_df.style.set_properties(**{"width": "100px"}), height=200)
 
-st.write("OS Properties")
-st.dataframe(os_properties, width=600, height=300)
+# Display memory properties table
+st.write("## Memory Properties")
+st.dataframe(memory_properties_df.style.set_properties(**{"width": "100px"}), height=200)
 
-st.write("Hardware Properties")
-st.dataframe(hardware_properties, width=600, height=300)
-
-st.write("Logical Properties")
-st.dataframe(logical_properties, width=600, height=300)
-
-st.write("Locale Properties")
-st.dataframe(locale_properties, width=600, height=300)
-
-st.write("Memory Properties")
-st.dataframe(memory_properties, width=600, height=300)
+# Display network properties table
+st.write("## Network & Network Adapter Properties")
+st.dataframe(network_properties_df.style.set_properties(**{"width": "100px"}), height=600)
