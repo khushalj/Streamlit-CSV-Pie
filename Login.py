7f15cff3ac83c507bab05e0178e850ff647d295b
Shrey Kansal <skansal801@gmail.com>
2023-08-08 21:02:41 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -31,8 +31,8 @@ def authenticate(username, password):
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
-        return None
-    return r.json()
+         return None
+    return r.json()
 
 st.title("Login")
 
